9ac9ec6c2847f19d4a62d8d65b03a71d601375f9
karthik01932 <karthiksrinivas2001>
2024-01-19 22:56:59 +0530

    delete button

--- controllers/admin.js
+++ controllers/admin.js
@@ -64,3 +64,9 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+exports.postDeleteProduct = (req, res, next) =>{
+  const prodId = req.body.productId;
+  Product.deleteId(prodId);
+  res.redirect('/admin/products')
+}
